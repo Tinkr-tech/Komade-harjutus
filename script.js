5a4add2d925be06c805da757015363a99302763d
Krister Viirsaar <git@krister.ee>
2021-11-28 15:49:08 +0000

    Kas sõnas on koma

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ console.log('words', words)
 
 const newWords = []
 for (const word of words) {
+    // kas on koma?
+    const isComma = word.includes(',')
+    console.log(word, isComma)
+
     newWords.push(word)
     newWords.push('<input>')
 }
